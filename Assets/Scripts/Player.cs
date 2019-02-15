3ae5d513526aaf37f83552d03b6923b0af8bbe03
duckonthego <46766741+duckonthego@users.noreply.github.com>
2019-02-14 17:27:20 -0800

    Fbx and teleport added to main scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Vector3 playerPosition;       // Keeps track of where Player will be teleported.
+
+    [SerializeField]
+    float playerHeight = 1.8f;
+
+    void OnEnable()
+    {
+        TeleportManager.DoTeleport += MoveTo;
+    }
+
+    void OnDisable()
+    {
+        TeleportManager.DoTeleport -= MoveTo;
+    }
+
+    void MoveTo(Transform destTransform)
+    {
+        // Set the new position.
+        playerPosition = destTransform.position;
+        // Player's eye level should be playerHeight above the new position.
+        playerPosition.y += playerHeight;
+        // Move Player.
+        transform.position = playerPosition;
+    }
+}
---
+++ Assets/Scripts/UIMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMovement : MonoBehaviour {
+
+    [SerializeField] private Transform m_UIElement;         // The transform of the UI to be affected.
+    [SerializeField] private Transform m_Camera;            // The transform of the camera.
+    [SerializeField] private float m_FollowSpeed = 10f;     // The speed with which the UI should follow the camera.
+
+    private float m_DistanceFromCamera;                     // The distance the UI should stay from the camera when rotating with it.
+
+    // Use this for initialization
+    void Start () {
+        m_DistanceFromCamera = Vector3.Distance(m_UIElement.position, m_Camera.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_UIElement.rotation = Quaternion.LookRotation(m_UIElement.position - m_Camera.position);
+
+        Quaternion headRotation = UnityEngine.XR.InputTracking.GetLocalRotation(UnityEngine.XR.XRNode.Head);
+        Vector3 headDirection = headRotation * Vector3.forward;
+        Vector3 headDirectionWithDistance = headDirection * m_DistanceFromCamera;
+        m_UIElement.position = m_Camera.position + headDirectionWithDistance;
+    }
+}
