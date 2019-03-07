8965d847c67bf5006dc20c264cd4495baff707f3
jqiufu <jqiufu@alumni.ubc.ca>
2019-03-06 19:02:09 -0800

    Audio+Scene

---
+++ Assets/Scripts/TalkingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingScript : MonoBehaviour {
+
+    Vector3 playerPosition;       // Keeps track of where Player will be teleported.
+    public SoundFXRef testSound1;
+
+    void OnEnable()
+    {
+        PlayAudioManagerObject.DoAudio += ListenTo;
+    }
+
+    void OnDisable()
+    {
+        PlayAudioManagerObject.DoAudio -= ListenTo;
+    }
+
+    void ListenTo(Transform destTransform)
+    {
+        // Set the new position.
+        playerPosition = destTransform.position;
+
+        testSound1.PlaySoundAt(playerPosition);
+    }
+}
+
